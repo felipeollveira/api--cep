fbd2343deab2df970986fc0f52694ea3033128df
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2023-10-02 13:24:06 -0300

    big update

--- src/App.js
+++ src/App.js
@@ -1,100 +1,39 @@
 
-import {useState} from 'react';
-import {FiSearch} from 'react-icons/fi';
-import {FiCopy} from 'react-icons/fi';
 import './App.css';
-import api from './services/api.js';
+import Modal from './componentes/modal.js';
+import './componentes/cepModal.css'
+import React, { useState } from 'react';
+
 
 
 function App() {
-
-
-
-  const [input, setInput] = useState('')
-  const [cep, setCep] = useState({})
-
   
-  async function Btn(){
-    if(input === ''){
-    alert("Digite algum CEP!")
-      return;
-    }try{
-      const response = await api.get(`${input}/json`)
-      if(response.data == 'true'){ 
-        alert('erro na busca.')
-      }
-      setCep(response.data)
-      console.log(response.data)
-      setInput("")
-      
-     
-    }catch{
-        alert("Erro na busca.")
-       
-         }
-  }
-        
-   
-
-    function copiar(){
-       const dados = `${cep.cep}: ` + `${cep.logradouro},` + ` ${cep.bairro},` 
-       + ` ${cep.localidade}-` + `${cep.uf}`;
-
-       navigator.clipboard.writeText(dados).then(() =>{
-        alert('Copiado para área de transferência')
-       }
-       )
-    }
-
-   
-
-
-
+  const [modalOpen, setModalOpen] = useState();
+  const handleBtnClick = () => {
+    setModalOpen(true)
+   };
+   const handleBtnFecha = () =>{
+    setModalOpen(false)
+   }
+  
   return (
-    <div className="container">
-      <h1 className='title'>Buscador de CEP</h1>
+    <div>
+      <div className='CEP'>
+        <button onClick={handleBtnClick}
+          className='btnCep' 
+          aria-required>
+          Busque um cep</button>
+          {modalOpen && <Modal />}
 
-      <div className='containerInput'>
-        <input
-      
-        type='text'
-        placeholder='Digite seu cep...'
-        autoComplete='off'
-        maxLength={8}
-        value={input}
-        onChange={(e) => setInput(e.target.value) } 
-        />
-
+        <span className='fechar' onClick={handleBtnFecha}>x</span>
         
-        <button className='buttonSearch' onClick={Btn}>
-          <FiSearch size={25} color=''/>
-        </button>
-      </div>
-
-   
-            <main >
-            
-                <h2>{cep.cep}</h2>
-                <section>
-                
-                      
-                  <span id='rua'>{cep.logradouro}</span>
-                  <span id='bairro'>{cep.bairro}</span>
-                  <span id='local'>{cep.localidade}
-                  {cep.uf}</span> 
-                  <span 
-                  className='copy'>
-                    <FiCopy
-                    size={15} 
-                    color='#fff' 
-                    onClick={copiar}>
-                    </FiCopy>
-                  </span>
-        </section>
-            </main>
         </div>
+        <div className='COMPRAS'>
+
+        </div>
+    </div>
+     
   );
 }
 
 export default App;
-
---
+++ src/componentes/btnFechar.js
@@ -0,0 +1,15 @@
+import React, { useState, useRef, useCallback } from 'react';
+import '../App.css';
+import './cepModal.css'
+
+function btnFechar(){
+
+
+return( 
+    <div> 
+    <span className='fechar'>x</span>
+    </div>
+)
+}
+
+export default btnFechar;
